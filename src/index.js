72d3b12f29f4ccec065f99dec7d70c957f4e1bb3
Wind-PC <shiow620412@gmail.com>
2024-08-18 22:54:46 +0800

    add tempo-devtools

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { TempoDevtools } from "tempo-devtools"
+TempoDevtools.init();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
